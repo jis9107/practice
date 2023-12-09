6337beb111879e2b15ab932bf002719631dcf29f
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-09 15:38:46 +0900

    Part1

--- Part1/Part1/Program.cs
+++ Part1/Part1/Program.cs
@@ -7,17 +7,7 @@ namespace Part1
     {
         static void Main(string[] args)
         {
-            Console.Write("이름 :");
-            string name = Console.ReadLine();
-            Console.Write("나이 :");
-            int age = int.Parse(Console.ReadLine());
-            Console.Write("키 :");
-            float height = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("안녕하세요" + name + "님");
-            Console.WriteLine("나이는 [age], 키는 {height}cm 이군요");
-            Console.WriteLine(name);
-            Console.WriteLine(age);
+            Console.WriteLine("{0:F3}", 1234.5);
 
         }
     }
