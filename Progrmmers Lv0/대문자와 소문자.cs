b8ebb1f39e9c4bea3d87250b6c7b7cfb7c57f848
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-09 16:34:30 +0900

    Part1

---
+++ Progrmmers Lv0/대문자와 소문자.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Solution
+{
+    public string solution(string my_string)
+    {
+        string answer = "";
+
+        for (int i = 0; i < my_string.Length; i++)
+        {
+            if (Char.IsLower(my_string[i]) == true)
+                answer += Char.ToUpper(my_string[i]);
+            else
+                answer += Char.ToLower(my_string[i]);
+        }
+
+        return answer;
+    }
+}
---
+++ Progrmmers Lv0/숨어어있는 숫자의 덧셈.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Solution
+{
+    public int solution(string my_string)
+    {
+        int answer = 0;
+
+        string s = Regex.Replace(my_string, @"\D", ""); // my_string에서 숫자만 추출
+        for (int i = 0; i < s.Length; i++)
+        {
+            answer += (int)s[i] - 48; // 아스키 코드 값 빼기
+        }
+
+
+        return answer;
+    }
+}
