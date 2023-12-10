e324e5dba1b95ad62d2061d9954e4513ec50f030
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-10 12:35:37 +0900

    Lv_0

---
+++ Progrmmers Lv0/외계 행성의 나이.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Solution
+{
+    public string solution(int age)
+    {
+        string answer = "";
+
+        string ag = age.ToString();
+
+        for (int i = 0; i < ag.Length; i++)
+        {
+            if (ag[i] == '0')
+                answer += 'a';
+
+            else if (ag[i] == '1')
+                answer += 'b';
+
+            else if (ag[i] == '2')
+                answer += 'c';
+
+            else if (ag[i] == '3')
+                answer += 'd';
+
+            else if (ag[i] == '4')
+                answer += 'e';
+
+            else if (ag[i] == '5')
+                answer += 'f';
+
+            else if (ag[i] == '6')
+                answer += 'g';
+
+            else if (ag[i] == '7')
+                answer += 'h';
+
+            else if (ag[i] == '8')
+                answer += 'i';
+
+            else if (ag[i] == '9')
+                answer += 'j';
+
+        }
+
+
+
+        return answer;
+    }
+}
