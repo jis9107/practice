661b6e5a1dacbafe09baca79c7bcbab79bac1f9a
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-11 14:13:31 +0900

    Lv_0

---
+++ Progrmmers Lv0/숫자 찾기.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int num, int k)
+    {
+        int answer = 0;
+
+        string numbers = num.ToString();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (int.Parse(numbers[i].ToString()) == k)
+            {
+                answer = i + 1;
+                break;
+            }
+            else
+                answer = -1;
+        }
+        return answer;
+
+        //int answer = num.ToString().IndexOf(k.ToString()) + 1;
+        //return answer == 0 ? answer - 1 : answer;
+    }
+}
