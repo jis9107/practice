bed9972b062adf392ce54c13c891152bf7fffa53
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-11 14:19:52 +0900

    Lv 0

---
+++ Progrmmers Lv0/A로 B만들기.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(string before, string after)
+    {
+        int answer = 0;
+
+        before = String.Concat(before.OrderBy(ch => ch));
+        after = String.Concat(after.OrderBy(ch => ch));
+
+        if (before == after)
+            answer = 1;
+        else
+            answer = 0;
+        return answer;
+    }
+}
