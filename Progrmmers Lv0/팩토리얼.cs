2dbc7f750c4847999158a22b9bcc4af9031cb03b
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-11 15:26:25 +0900

    Lv 0

---
+++ Progrmmers Lv0/팩토리얼.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 1;
+        int temp = 1;
+
+        while (true)
+        {
+            answer++;
+            temp *= answer;
+
+            if (temp > n)
+                return answer - 1;
+            if (temp == n)
+                return answer;
+        }
+
+    }
+}
