588aba730db316413f678d68eaf84a361d6e4ff9
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-12 17:27:32 +0900

    12/12

---
+++ Progrmmers Lv0/직사각형 넓이 구하기.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int[,] dots)
+    {
+        int answer = 0;
+
+        int[] width = new int[dots.GetLength(0)];
+        int[] height = new int[dots.GetLength(0)];
+
+        for (int i = 0; i < dots.GetLength(0); i++)
+        {
+            width[i] = dots[i, 0];
+            height[i] = dots[i, 1];
+        }
+
+        int maxWidth = width.Max();
+        int minWidth = width.Min();
+        int maxHeight = height.Max();
+        int minHeight = height.Min();
+
+
+        //가로 최대 - 가로 최소 = (가로 길이) 세로 최대 - 세로 최소 = (세로 길이)
+        return (maxWidth - minWidth) * (maxHeight - minHeight);
+    }
+}
