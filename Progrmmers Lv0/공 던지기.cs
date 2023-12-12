5f04b2bd51e4c1bd7665a8064b2441081b31109d
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-12 17:32:53 +0900

    12/12

---
+++ Progrmmers Lv0/공 던지기.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int[] numbers, int k)
+    {
+        int answer = 0;
+
+        for (int i = 0; i < k - 1; i++)
+        {
+            answer = numbers[i];
+        }
+
+        return answer;
+    }
+}
