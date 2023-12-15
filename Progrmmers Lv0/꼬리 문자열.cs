75e6c4d1646e4db67ca96b781d3375c682ea01af
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-15 15:49:44 +0900

    12/15

---
+++ Progrmmers Lv0/꼬리 문자열.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public string solution(string[] str_list, string ex)
+    {
+        string answer = "";
+
+        for (int i = 0; i < str_list.Length; i++)
+        {
+            if (!str_list[i].Contains(ex))
+                answer += str_list[i];
+        }
+        return answer;
+    }
+}
