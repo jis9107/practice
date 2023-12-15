9d3e85bfff9813f007f8a3399dce14233ac7733d
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-15 15:29:20 +0900

    12/15

---
+++ Progrmmers Lv0/특정한 문자를 대문자로 바꾸기.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public string solution(string my_string, string alp)
+    {
+
+        my_string = my_string.Replace(alp, alp.ToUpper());
+
+        return my_string;
+    }
+}
