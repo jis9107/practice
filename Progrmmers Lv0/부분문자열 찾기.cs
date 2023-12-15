627b5b12e2becde311df95ab2610f95a880b6c8a
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-15 15:45:31 +0900

    12/15

---
+++ Progrmmers Lv0/부분문자열 찾기.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+
+public class Solution
+{
+    public int solution(string my_string, string target)
+    {
+
+        if (my_string.Contains(target))
+            return 1;
+        else
+            return 0;
+
+    }
+}
