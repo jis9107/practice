46804d98d63e51f3ca391f866a30f190cb5dd3db
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-15 16:33:58 +0900

    12/15

---
+++ Progrmmers Lv0/뒤에서 5등 위로.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public int[] solution(int[] num_list)
+    {
+        int[] answer = new int[num_list.Length - 5];
+
+        Array.Sort(num_list);
+
+        for (int i = 0; i < answer.Length; i++)
+        {
+            answer[i] = num_list[i + 5];
+        }
+        return answer;
+    }
+}
