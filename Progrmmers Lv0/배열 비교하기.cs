0e4eaaa259414b076db374bb4ee52c5da833d237
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-15 17:04:35 +0900

    12/15

---
+++ Progrmmers Lv0/배열 비교하기.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public class Solution
+{
+    public int solution(int[] arr1, int[] arr2)
+    {
+        int answer = 0;
+
+        if (arr1.Length > arr2.Length)
+            return 1;
+        else if (arr1.Length < arr2.Length)
+            return -1;
+        else
+        {
+            int arr1_sum = arr1.Sum();
+            int arr2_sum = arr2.Sum();
+
+            if (arr1_sum > arr2_sum)
+                return 1;
+            else if (arr1_sum < arr2_sum)
+                return -1;
+            else
+                return 0;
+
+        }
+
+    }
+}
