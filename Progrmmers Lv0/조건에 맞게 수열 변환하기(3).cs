805738a94607fa8430487287acbb3b71580c3009
jis9107 <117075698+jis9107@users.noreply.github.com>
2023-12-15 16:47:46 +0900

    12/15

---
+++ Progrmmers Lv0/조건에 맞게 수열 변환하기(3).cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] solution(int[] arr, int k)
+    {
+        int[] answer = new int[arr.Length];
+
+        if (k % 2 == 1) // K는 홀수
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                answer[i] = arr[i] * k;
+            }
+        }
+
+        else if (k % 2 == 0) // K는 짝수
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                answer[i] = arr[i] + k;
+            }
+        }
+
+        return answer;
+    }
+}
