3f1c88f7fb0eefff6bc2ce660433743cb156865d
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-03-04 17:05:00 +0900

    practice

---
+++ Programmers Lv2/Programmers Lv2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class Solution
+{
+    public int solution(int n)
+    {
+
+        string num1 = Convert.ToString(n, 2);
+        int freq = num1.Count(x => (x == '1'));
+        int temp = n;
+        int freq2 = 0;
+
+        while (freq != freq2)
+        {
+            temp++;
+            string num2 = Convert.ToString(temp, 2);
+            freq2 = num2.Count(x => (x == '1'));
+        }
+
+        return temp;
+    }
+}
